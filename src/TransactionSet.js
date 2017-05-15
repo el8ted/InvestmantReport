9a86d1e81e4cde302944a67ea6d3d6cba690cf12
Tom <tomek32@gmailc.om>
2017-05-15 18:48:18 -0400

    Refactor 1. Changed getOrderTransaction to accept Security class, not string of SecurityUID 2. Removed unnessary null assignment to UID

--- src/TransactionSet.js
+++ src/TransactionSet.js
@@ -3,58 +3,44 @@
  */
 "use strict";
 
-
-// Configuration to run on node with mock data
-global.RUN_ON_NODE = true;
-if (global.RUN_ON_NODE) {
-  var InvestmentType = require('./InvestmentType.js').InvestmentType;
-
-  module.exports.TransactionSet = TransactionSet;
-}
-
-
 /**
- * Object to hold transactions by category
+ * Class to hold list of transactions, categorized by investment type
  */
 function TransactionSet() {
   this.transactionsList = {'INTEREST': [], 'CARRY_CHARGE': [], 'DIVIDEND': [], 'ORDER': [], 'OPTION_ORDER': []};
 }
 
-
 /**
- * @param transaction - Transaction object to add to list
+ * @param {Transaction} transaction
  */
 TransactionSet.prototype.addTransaction = function(transaction) {
   this.transactionsList[transaction.getInvestmentType()].push(transaction);
 };
 
-
 /**
- * @returns array of transactions objects of type dividend
- */
-TransactionSet.prototype.getDividendTransactions = function() {
-  return this.transactionsList[InvestmentType.DIVIDEND];
-};
-
-
-/**
- * @returns array of transactions objects of type interest
- */
-TransactionSet.prototype.getInterestTransactions = function() {
-  return this.transactionsList[InvestmentType.INTEREST];
-};
-
-
-/**
- *@returns array of transactions objects of type carry charge
+ * @returns {[CarryChargeTransaction]}
  */
 TransactionSet.prototype.getCarryChargeTransactions = function() {
   return this.transactionsList[InvestmentType.CARRY_CHARGE];
 };
 
+/**
+ * @returns {[DividendTransaction]}
+ */
+TransactionSet.prototype.getDividendTransactions = function() {
+  return this.transactionsList[InvestmentType.DIVIDEND];
+};
 
 /**
- * @returns array of transactions objects of type order
+ * @returns {[InterestTransaction]}
+ */
+TransactionSet.prototype.getInterestTransactions = function() {
+  return this.transactionsList[InvestmentType.INTEREST];
+};
+
+/**
+ * @param {Security} security - optional
+ * @returns {[OrderTransaction]]} if security is specified, returns list matching only security
  */
 TransactionSet.prototype.getOrderTransactions = function(security) {
   var orders = [];
@@ -63,7 +49,7 @@ TransactionSet.prototype.getOrderTransactions = function(security) {
     return this.transactionsList[InvestmentType.ORDERS];
   else {
     for (var i = 0; i < this.transactionsList[InvestmentType.ORDERS].length; i++) {
-      if (security === this.transactionsList[InvestmentType.ORDERS][i].getSecurity().getUID())
+      if (security.getUID() === this.transactionsList[InvestmentType.ORDERS][i].getSecurity().getUID())
         orders.push(this.transactionsList[InvestmentType.ORDERS][i]);
     };
   }
@@ -71,29 +57,42 @@ TransactionSet.prototype.getOrderTransactions = function(security) {
   return orders;
 };
 
-
 /**
- * @returns array of transactions objects of type option order
+ * @param {Security} security - optional
+ * @returns {[OptionOrderTransaction]]} if security is specified, returns list matching only security
  */
-TransactionSet.prototype.getOptionOrderTransaction = function(transaction) {
+TransactionSet.prototype.getOptionOrderTransaction = function(security) {
+  //TODO: need to implement
   return this.transactionsList[InvestmentType.OPTION_ORDER];
 };
 
-
 /**
- * @param type - string of transaction type (INTEREST, CARRY_CHARGE, DIVIDEND, ORDER, OPTION_ORDER)
- * @returns json object {'ACCOUNT CURRENT', 'SECURITY_ID'} of unqiue indenitifers matching the transaction type specified
+ * @param {InvestmentType}
+ * @returns {[Security]} unique list matching the investment type
  */
 TransactionSet.prototype.getUniqueSecurities = function (type) {
-  var list = [], UID;
+  var list = {UIDs: [], securities = []};
+  var UID;
 
   for (var i = 0; i < this.transactionsList[type].length; i++) {
-    UID = null;
-    UID = this.transactionsList[type][i].getSecurity().getUID();
+    var security = this.transactionsList[type][i].getSecurity();
+    //UID = null;
+    UID = security.getUID();
 
-    if (list.indexOf(UID) === -1)
-      list.push(UID);
+    if (list.UIDs.indexOf(UID) === -1) {
+      list.UIDs.push(UID);
+      list.securities.push(security);
+    }
   }
 
-  return list;
+  return list.securities;
 };
+
+
+// Configuration to run on node with mock data
+global.RUN_ON_NODE = true;
+if (global.RUN_ON_NODE) {
+  var InvestmentType = require('./InvestmentType.js').InvestmentType;
+
+  module.exports.TransactionSet = TransactionSet;
+}
