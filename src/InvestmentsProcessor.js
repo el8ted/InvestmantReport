95aad7ce9aa76e4656430f9d3df88731fa027990
Tom <tomek32@gmailc.om>
2017-05-15 23:31:49 -0400

    noissue

--- src/InvestmentsProcessor.js
+++ src/InvestmentsProcessor.js
@@ -3,174 +3,127 @@
  */
 "use strict";
 
-
 /**
  * InvestmentsProcessor class
  */
 function InvestmentsProcessor() {
-  
   this.transactionsSet = new TransactionSet();
 }
 
-
 /**
- * @param transaction
+ * @param {Parent of BaseTransaction} transaction
  */
 InvestmentsProcessor.prototype.addTransaction = function (transaction) {
   this.transactionsSet.addTransaction(transaction);
 };
 
-
 /**
- * @return {array}
+ * @param {Security} security
+ * @return {array} of CarryChargeTransactions
  */
-InvestmentsProcessor.prototype.getCarryChargeSecurities = function () {
+InvestmentsProcessor.prototype.getCarryChargeBySecurity = function (security) {
   return this.transactionsSet.getCarryChargeTransactions();
 };
 
-
 /**
- * @return {array}
+ * @param {Security} security
+ * @return {array} of InterestTransactions
  */
-InvestmentsProcessor.prototype.getInterestSecurities = function () {
+InvestmentsProcessor.prototype.getInterestBySecurity = function (security) {
   return this.transactionsSet.getInterestTransactions();
 };
 
-
 /**
- * @return {Array}
- */
-InvestmentsProcessor.prototype.getRealizedGainLossSecurities = function () {
-  var securityIDs = this.transactionsSet.getUniqueSecurities(InvestmentType.ORDERS);
-  var securityKey;
-  var gainLossReport = [], gainLossSecurity = [];
-
-  // TODO: add option order transaction
-  for (securityKey in securityIDs)
-    if (securityIDs.hasOwnProperty(securityKey)) {
-      gainLossSecurity = [];
-      gainLossSecurity.push(securityIDs[securityKey]);
-      gainLossSecurity.push(this.getRealizedGainLossBySecurity(securityIDs[securityKey]));
-
-      gainLossReport.push(gainLossSecurity);
-    }
-
-  return gainLossReport;
-};
-
-
-/**
- * @param security
- * @return {[*,*,*]}
+ * @param {Security} security
+ * @returns {{totalGainLoss, totalQuantity, totalBookValue}}
  */
 InvestmentsProcessor.prototype.getRealizedGainLossBySecurity = function (security) {
   var orders = this.transactionsSet.getOrderTransactions(security);
-  var totalQuantity = 0, totalBookValue = 0, avgCostPerQuantity = 0, totalGainLoss = 0;
-  var nextDate, date, amount = 0, quantity = 0, gainLoss = 0, usdRate = null;
+  var total = {bookValue: 0, gainLoss: 0, quantity: 0, ACB: 0};
+  var order = {date: null, amount: 0, quantity: 0, usdRate: null};
+  var avgCostPerQuantity = 0, nextDate = null, gainLoss = 0; 
 
   for (var j = 0; j < orders.length; j++) {
-    if (j + 1 < orders.length)
+    if ((j + 1) < orders.length)
       nextDate = orders[j + 1].getTradeDate();
     else
       nextDate = null;
 
-    date = orders[j].getTradeDate();
-    amount = Number(orders[j].getAmount());
-    quantity = Number(orders[j].getQuantity());
-    usdRate = (orders[j].getUSDRate()) ? Number(orders[j].getUSDRate()): null;
+    order.amount = Number(orders[j].getAmount());
+    order.quantity = Number(orders[j].getQuantity());
+    order.usdRate = (orders[j].getUSDRate()) ? Number(orders[j].getUSDRate()): null;
 
     // Convert USD values to CAD
     if (usdRate !== null)
       amount = amount * usdRate;
 
     // Catch rounding errors
-    totalQuantity = totalQuantity + quantity;
-    if (Math.abs(totalQuantity) < 0.001)
-      totalQuantity = 0;
+    total.quantity = total.quantity + order.quantity;
+    if (Math.abs(total.quantity) < 0.001)
+      total.quantity = 0;
 
     // Record gain/loss for closing transactions (sell, or buy to cover)
-    if ((quantity < 0 && totalQuantity >= 0) || (quantity > 0 && totalQuantity <= 0)) {
-      gainLoss = (quantity * avgCostPerQuantity) + amount;
+    if (((order.quantity < 0) && (total.Quantity >= 0)) || ((order.quantity > 0) && (total.quantity <= 0))) {
+      gainLoss = (order.quantity * total.ACB ) + order.amount;
 
-      if (totalQuantity === 0 || nextDate === null || gainLoss >= 0 || (gainLoss < 0 && isBoughtBackWithin30Days(orders, j) === false))
-        totalGainLoss = totalGainLoss + gainLoss;
+      if ((total.quantity === 0) || (nextDate === null) || (gainLoss >= 0) ||
+          ((gainLoss < 0) && (isBoughtBackWithin30Days(orders, j) === false)))
+        total.gainLoss = total.gainLoss + gainLoss;
     }
 
-    totalBookValue = this.getUpdatedTotalBookValue(orders, totalQuantity, totalBookValue, avgCostPerQuantity, date, quantity, amount, nextDate, gainLoss, j);
-    if (totalQuantity !== 0)
-      avgCostPerQuantity = totalBookValue / totalQuantity;
+    total.bookValue = this.getUpdatedTotalBookValue(orders, total, order, nextDate, gainLoss, j);
+    if (total.quantity !== 0)
+      total.ACB = total.bookValue / total.quantity;
     else
-      avgCostPerQuantity = 0;
+      total.ACB  = 0;
   }
 
-  return [totalGainLoss, totalQuantity, totalBookValue];
+  return {'totalGainLoss': totalGainLoss, 'totalQuantity': totalQuantity, 'totalBookValue': totalBookValue};
 };
 
+/**
+ * @returns {array} of unique securities of type Security
+ */
+InvestmentsProcessor.prototype.getUniqueSecurities = function() {
+  return this.transactionsSet.getUniqueSecurities();
+}
 
 /**
- * @param orders
- * @param totalQuantity
- * @param totalBookValue
- * @param avgCostPerQuantity
- * @param date
- * @param quantity
- * @param amount
- * @param nextDate
- * @param gainLoss
- * @param n
- * @return {*}
+ * @param  {[OrderTransaction]} orders
+ * @param  {{number, number, number}} total {quantity, bookValue, avgCostPerQuantity}
+ * @param  {{string, number}} {date, quantity, amount}
+ * @param  {string} nextDate
+ * @param  {number} gainLoss
+ * @param  {number} n
+ * @returns  {number} new book value after processing transactions
  */
-InvestmentsProcessor.prototype.getUpdatedTotalBookValue = function (orders, totalQuantity, totalBookValue, avgCostPerQuantity, date, quantity, amount, nextDate, gainLoss, n) {
+InvestmentsProcessor.prototype.getUpdatedTotalBookValue = function (orders, total, order, nextDate, gainLoss, n) {
   // Expiring contract or transaction closes entire position
-  if (quantity !== 0 && totalQuantity === 0)
-    totalBookValue = 0;
+  if (quantity !== 0 && total.quantity === 0)
+    total.bookValue = 0;
   // Buy action
-  else if (quantity > 0 && totalQuantity >= 0)
+  else if (quantity > 0 && total.quantity >= 0)
     totalBookValue = totalBookValue - amount;
   // Cover transaction
-  else if (quantity < 0 && totalQuantity < 0)
-    totalBookValue = totalBookValue - amount;
+  else if (quantity < 0 && total.quantity < 0)
+    total.bookValue = total.bookValue - amount;
   // Sell action
   else {
     // Calculation is based on superfical loss rule
-    totalBookValue = totalBookValue + (quantity * avgCostPerQuantity);
+    total.bookValue = total.bookValue + (quantity * this.ACB);
 
-    if ((totalQuantity !== 0) && isBoughtBackWithin30Days(orders, n) && (gainLoss < 0))
-      totalBookValue = totalBookValue - gainLoss;
+    if ((total.quantity !== 0) && isBoughtBackWithin30Days(orders, n) && (gainLoss < 0))
+      total.bookValue = total.bookValue - gainLoss;
   }
 
-  return totalBookValue;
+  return total.bookValue;
 };
 
-
 /**
- * Performs gain/loss calculation
- * @param totalQuantity
- * @param totalBookValue
- * @param avgCostPerQuantity
- * @param date
- * @param quantity
- * @param amount
- */
-function calculateGainLoss(totalQuantity, totalBookValue, avgCostPerQuantity, date, quantity, amount) {
-  var gainLoss = 0, totalGainLoss = 0;
-
-  if (quantity < 0) {
-    gainLoss = (quantity * avgCostPerQuantity) + amount;
-    this.orders[j][6] = gainLoss;
-    totalGainLoss = totalGainLoss + gainLoss;
-  }
-
-  avgCostPerQuantity = totalBookValue / totalQuantity;
-}
-
-
-/**
- * return true if bought back within 30 calendar days
  * FIXME: check for short covers and buy backs in a future year
- * @param orders
- * @param fromIndex
- * @return {boolean}
+ * @param {[OrderTransaction]} orders
+ * @param {number} fromIndex
+ * @returns {boolean} true if bought back within 30 calendar days fromIndex in orders
  */
 function isBoughtBackWithin30Days(orders, fromIndex) {
   var fromDate = new Date(orders[fromIndex][0]);
@@ -200,5 +153,32 @@ if (global.RUN_ON_NODE) {
   var TransactionSet = require('./TransactionSet.js').TransactionSet;
   var InvestmentType = require('./InvestmentType.js').InvestmentType;
 
+  var InvestmentsReportModule = require('./InvestmentsReport.js');
+  InvestmentsReport = InvestmentsReportModule.InvestmentsReport;
+  CarryChargeSecurity = InvestmentsReportModule.CarryChargeSecurity;
+  DividendSecurity = InvestmentsReportModule.DividendSecurity;
+  OrderSecurity = InvestmentsReportModule.OrderSecurity;
+
   module.exports = InvestmentsProcessor;
 }
+
+/**
+ * Performs gain/loss calculation
+ * @param totalQuantity
+ * @param totalBookValue
+ * @param avgCostPerQuantity
+ * @param date
+ * @param quantity
+ * @param amount
+ *
+function calculateGainLoss(totalQuantity, totalBookValue, avgCostPerQuantity, date, quantity, amount) {
+  var gainLoss = 0, totalGainLoss = 0;
+
+  if (quantity < 0) {
+    gainLoss = (quantity * avgCostPerQuantity) + amount;
+    this.orders[j][6] = gainLoss;
+    totalGainLoss = totalGainLoss + gainLoss;
+  }
+
+  avgCostPerQuantity = totalBookValue / totalQuantity;
+}*/
